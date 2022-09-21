e549e9833cb8d89dd03205f58ae8fd93d180d42e
keerthana <keerthana.v@dorustree.in>
2022-09-21 19:21:58 +0530

    angular meterials

---
+++ bars/bar_components/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HelloComponent } from './hello.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HelloComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
